90a219e7188dd56eabe9ed57c48bf00d4cce1e59
Nicolás Exequiel Ferragamo <nferragamo@frba.utn.edu.ar>
2024-06-15 23:45:48 +0000

    ejemplo_practica_2

--- main.cpp
+++ main.cpp
@@ -6,24 +6,11 @@
 #include "mbed.h"
 
 // Create a DigitalOutput object to toggle an LED whenever data is received.
-static DigitalOut led(LED1);
+DigitalOut led(LED1);
 
 // Create a UnbufferedSerial object with a default baud rate.
-static UnbufferedSerial serial_port(USBTX, USBRX);
+UnbufferedSerial serial_port(USBTX, USBRX);
 
-void on_rx_interrupt()
-{
-    char c;
-
-    // Toggle the LED.
-    led = !led;
-
-    // Read the data to clear the receive interrupt.
-    if (serial_port.read(&c, 1)) {
-        // Echo the input back to the terminal.
-        serial_port.write(&c, 1);
-    }
-}
 
 int main(void)
 {
@@ -34,7 +21,14 @@ int main(void)
         /* parity */ SerialBase::None,
         /* stop bit */ 1
     );
-
-    // Register a callback to process a Rx (receive) interrupt.
-    serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
+    char c;
+    while(true)
+    {
+        if (serial_port.read(&c, 1))
+        {
+            led = !led;
+            serial_port.write("#", 1);        
+        }
+    }
+    
 }
